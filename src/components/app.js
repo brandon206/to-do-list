76cea44c07c042b5f3bc464af3de91e0de9efa20
Brandon Park <brandonpark206@gmail.com>
2018-11-06 17:15:36 -0800

    completed add item functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,49 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
+import 'materialize-css/dist/js/materialize';
+import '../assets/css/app.css';
+import React, {Component} from 'react';
 import List from './list';
+import AddItem from './add_item';
+import listData from '../dummy_data/list';
+import { randomString } from '../helpers'; //when your file is called 'index' you don't need to specify anything
 
 
-const App = () => (
-    <div className = "container">
-        <h1 className = "center">To Do List</h1>
-        <List/>
-    </div>
-);
+class App extends Component{
+    constructor (props){
+        super(props);
+
+        this.state = {
+            list: []
+        }
+    }
+
+    addItem = (item) => {
+        item._id = randomString(8);
+        this.setState ({
+            list: [item, ...this.state.list]
+        });
+    }
+    
+    componentDidMount () {
+        this.getListData();
+    }
+
+    getListData () {
+        // Call server to get data
+        this.setState({
+            list: listData
+        });
+    }
+
+    render () {
+        return(
+            <div className = "container">
+                <h1 className = "center">To Do List</h1>
+                <AddItem add = {this.addItem}/>
+                <List data = {this.state.list}/>
+            </div>
+        );
+    }
+}
 
 export default App;
